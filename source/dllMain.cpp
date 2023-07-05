a24f2d140394826fb0fa7e25ae797b295dac7640
DesperatelyDC10 <374948482@qq.com>
2023-07-05 18:09:41 +0800

    DllMain's create

---
+++ source/dllMain.cpp
@@ -0,0 +1,18 @@
+#include <windows.h>
+
+bool APIENTRY DllMain(
+    HMODULE hModule,
+    DWORD ul_reason_for_call,
+    LPVOID lpReversed
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
